702bc03079bdbfda08db28f701ef131b9ae0ae5e
Patricio2308 <patricioandres00@gmail.com>
2023-06-30 12:14:34 -0300

    Se agrega la clase Moneda

---
+++ src/Moneda.java
@@ -0,0 +1,20 @@
+public class Moneda {
+    private String nombre;
+    private String simbolo;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getSimbolo() {
+        return simbolo;
+    }
+
+    public void setSimbolo(String simbolo) {
+        this.simbolo = simbolo;
+    }
+}
